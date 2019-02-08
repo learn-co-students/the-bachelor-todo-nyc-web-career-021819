ce60e70a8a13e507bcda1eefb84c09101b387afd
Adam Gordon <adamjgordon24@gmail.com>
2019-02-08 07:40:21 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,67 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |show_season, contestant|
+   if show_season == season 
+     contestant.each do |info|
+       info.each do |aspect, reading|
+         if info[aspect] == "Winner"
+           name_array = info["name"].split(" ")
+           return name_array[0]
+         end
+       end
+     end
+   end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |show_season, contestant|
+    contestant.each do |info|
+      info.each do |aspect, reading|
+        if info["occupation"] == occupation
+          return info["name"]
+        end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0  
+  data.each do |show_season, contestant|
+    contestant.each do |info|
+        if info["hometown"] == hometown
+          counter += 1 
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  counter = 0 
+  if counter < 1 
+    data.each do |show_season,contestant|
+      contestant.each do |info|
+        if info["hometown"] == hometown
+          return info["occupation"]
+         counter += 1
+       end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+age = 0
+count = 0
+  data[season].each do |info|
+    info.each do |aspect,reading|
+      if aspect == "age"
+        count += 1
+        age += reading.to_f
+      end
+    end
+  end
+answer = (age/count).round
+return answer
 end
